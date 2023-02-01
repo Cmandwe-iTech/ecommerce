62b0ddbb80fde8b72d9f09212c4feff002a63b3b
Chhatrapati Mandwe <mandwec12@gmail.com>
2023-02-01 20:46:11 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
 function App() {
+  const [data, setData] = useState([]);
+  const [count, setcount] = useState(0);
+  const [search, setsearch] = useState("smartphones")
+  const [hover, sethover] = useState(false)
+  useEffect(() => {
+    fetch(`https://dummyjson.com/products/category/${search}?skip=${(count)*1}&limit=4`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setData(data.products);
+      });
+  }, [count,search]);
+  const Counhandlerprev=()=>{
+    if(count > 0){
+      setcount(count - 1)
+    }
+  }
+  const Counthandlernext=()=>{
+    if(count < 2){
+      setcount(count + 1)
+    }
+  }
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="data-container">
+      <div style={{"textAlign":"center",margin:"20px"}}>
+        <select onChange={(e)=>setsearch(e.target.value)}>
+          <option selected>smartphones</option>
+          <option>laptops</option>
+          <option>fragrances</option>
+          <option>skincare</option>
+          <option>groceries</option>
+          <option>home-decoration</option>
+          <option>furniture</option>
+          <option>tops</option>
+          <option>womens-dresses</option>
+          <option>womens-shoes</option>
+          <option>mens-shirts</option>
+          <option>mens-shoes</option>
+          <option>mens-watches</option>
+          <option>womens-watches</option>
+          <option>womens-bags</option>
+          <option>womens-jewellery</option>
+          <option>sunglasses</option>
+          <option>automotive</option>
+          <option>motorcycle</option>
+          <option>lighting</option>
+        </select>
+      </div>
+      <div style={{display:"flex"}}>
+        {data.map((item, i) => {
+          return (
+            <span key={i} style={{"height":"300px","width":"300px","border":"2px solid black",margin:"10px auto 0",justifyContent:"space-around",display:"flex",background:"#cccc",textAlign:"center",flexDirection:"row"}}>
+              {hover?<span><img src={item.thumbnail} alt="thumbnails" style={{"height":"150px","width":"150px",margin:"20px"}} onClick={()=>{sethover(false)}}/><span>{item.description}</span></span>:
+              <img src={item.thumbnail} alt="thumbnails" style={{"height":"250px","width":"250px",margin:"20px"}} onClick={()=>{sethover(true)}}/>}
+            </span>  
+          );
+        })}
+      </div>
+      <div style={{"textAlign":"center",margin:"50px"}}>
+      <button onClick={Counhandlerprev}>prev</button><span>{count+1}</span><button onClick={Counthandlernext}>next</button>
+      </div>
     </div>
   );
 }
