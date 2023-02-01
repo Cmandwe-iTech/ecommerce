77134a391f13e64420754bef6b61fd3211a9b077
Chhatrapati Mandwe <mandwec12@gmail.com>
2023-02-01 21:07:30 +0530

    done

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ function App() {
         {data.map((item, i) => {
           return (
             <span key={i} style={{"height":"300px","width":"300px","border":"2px solid black",margin:"10px auto 0",justifyContent:"space-around",display:"flex",background:"#cccc",textAlign:"center",flexDirection:"row"}}>
-              {hover?<span><img src={item.thumbnail} alt="thumbnails" style={{"height":"150px","width":"150px",margin:"20px"}} onClick={()=>{sethover(false)}}/><span>{item.description}</span></span>:
+              {hover?<span onClick={()=>{sethover(false)}}><img src={item.thumbnail} alt="thumbnails" style={{"height":"150px","width":"150px",margin:"20px"}}/><span>{item.description}</span></span>:
               <img src={item.thumbnail} alt="thumbnails" style={{"height":"250px","width":"250px",margin:"20px"}} onClick={()=>{sethover(true)}}/>}
             </span>  
           );
